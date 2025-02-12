456af1eca26783b2a3d167117b13fe8639f0ce44
deepak <rajputdeepak5389@gmail.com>
2025-02-12 23:40:18 +0530

    wip

--- src/components/sendQuerys/ProductPage.jsx
+++ src/components/sendQuerys/ProductPage.jsx
@@ -78,46 +78,7 @@ const ProductPage = () => {
           
 
 
-          <div className="grid grid-cols-2 gap-6 p-6 border rounded-xl shadow-sm bg-white">
-            {/* Size Selection Box cursor-not-allowed opacity-30 */}
-            <div className="p-4 border rounded-lg shadow-sm ">
-              <h2 className="text-gray-700 font-bold mb-2">Select Size</h2>
-              <div className="flex space-x-6">
-                {sizes.map((size) => (
-                  <button
-                    key={size}
-                    onClick={() => setSelectedSize(size)}
-                    className={`px-4 py-2 border rounded-md transition-all ${
-                      selectedSize === size
-                        ? "border-blue-500 bg-blue-100"
-                        : "border-gray-300 hover:border-blue-400"
-                    }`}
-                  >
-                    {size}
-                  </button>
-                ))}
-              </div>
-            </div>
-
-            {/* Color Selection Box */}
-            <div className="p-4 border rounded-lg shadow-sm ">
-              <h2 className="text-gray-700 font-bold mb-2">Select Color</h2>
-              <div className="flex space-x-6">
-                {colors.map((color) => (
-                  <button
-                    key={color.name}
-                    onClick={() => setSelectedColor(color.name)}
-                    style={{ backgroundColor: color.code }}
-                    className={`w-10 h-10 rounded-full border-2 transition-all ${
-                      selectedColor === color.name
-                        ? "border-blue-500 scale-110"
-                        : "border-gray-300 hover:border-gray-500"
-                    }`}
-                  />
-                ))}
-              </div>
-            </div>
-          </div>
+        
 
 
 
