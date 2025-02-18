7ad6f2b1ed33bf7cfeb16e59bff7910b97f63235
deepak <rajputdeepak5389@gmail.com>
2025-02-18 15:28:35 +0530

    wip

--- src/components/HomePage/AvailablePlatforms.jsx
+++ src/components/HomePage/AvailablePlatforms.jsx
@@ -3,9 +3,9 @@ import StatsSection from "./StatsSection"; // Import the StatsSection component
 
 const platforms = [
   { name: "Amazon", url: "https://www.amazon.in/", image: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" },
-  { name: "Flipkart", url: "https://www.flipkart.com/", image: "https://s.yimg.com/fz/api/res/1.2/i155bcyO_F7niI0vCmXN_A--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MzMy/https://s.yimg.com/zb/imgv1/9adafff1-85af-34ef-95cc-a848ad6b4a4d/t_500x300" },
   { name: "Shopshy", url: "https://www.shopsy.in/", image: "https://tse3.mm.bing.net/th?id=OIP.BCqrjFCZvJAxbLkqyclp2AHaCS&pid=Api&P=0&h=180" },
-  { name: "Myntra", url: "https://www.myntra.com/", image: "https://tse2.mm.bing.net/th?id=OIP.bgzOO_V_Y79YKSEobt0dxwHaCU&pid=Api&P=0&h=180" }    
+  { name: "Myntra", url: "https://www.myntra.com/", image: "https://tse2.mm.bing.net/th?id=OIP.bgzOO_V_Y79YKSEobt0dxwHaCU&pid=Api&P=0&h=180" },    
+  { name: "Flipkart", url: "https://www.flipkart.com/", image: "https://s.yimg.com/fz/api/res/1.2/i155bcyO_F7niI0vCmXN_A--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MzMy/https://s.yimg.com/zb/imgv1/9adafff1-85af-34ef-95cc-a848ad6b4a4d/t_500x300" },
 ];
 
 const AvailablePlatforms = () => {
@@ -16,7 +16,7 @@ const AvailablePlatforms = () => {
         <div className="mt-8 flex flex-wrap justify-center gap-8">
           {platforms.map((platform) => (
             <a key={platform.name} href={platform.url} target="_blank" rel="noopener noreferrer" className="block bg-white p-4 rounded-lg  hover:shadow-xl transition">
-              <img src={platform.image} alt={platform.name} className="h-10 w-44 mx-auto" />
+              <img src={platform.image} alt={platform.name} className="h-12 w-auto mx-auto" />
             </a>
           ))}
         </div>
